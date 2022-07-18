d4a91458ce35394eec68e94cd43641d3b8b51f17
Tharusha Dilshan <109469473+tharushadilshan2006@users.noreply.github.com>
2022-07-18 09:01:46 +0530

    Update queen shashi.js

--- queen shashi.js
+++ queen shashi.js
@@ -1,1 +1,73 @@
 
+
+//═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                                                                     //
+//                                                                 ＱＵＥＥＮ ＳＨＡＳＨＩ💃❤️                                                                    //
+//                                                                                                                                                     //
+//                                                                    Ｖ：１．０．１                                                                     //
+//                                                                                                                                                     //
+//                                                                                                                                                                                          //
+//                                                                                                                                                     //
+//═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════//
+
+//coded by Tharusha Dilshan 
+
+
+const
+	{
+		WAConnection,
+		MessageType,
+		Presence,
+		MessageOptions,
+		Mimetype,
+		WALocationMessage,
+		WA_MESSAGE_STUB_TYPES,
+		WA_DEFAULT_EPHEMERAL,
+		ReconnectMode,
+		ProxyAgent,
+		GroupSettingChange,
+		waChatKey,
+		mentionedJid,
+		processTime,
+	} = require("@adiwajshing/baileys")
+const fs = require("fs")          
+const axios = require('axios')
+const hx = require('hxz-api')
+const os = require('os')
+const speed = require("performance-now")
+const util = require('util')
+const crypto = require('crypto')
+const request = require('request')
+const { exec, spawn } = require('child_process')
+const fetch = require('node-fetch')
+const moment = require('moment-timezone')
+const ffmpeg = require('fluent-ffmpeg')
+const simple = require('./lib/simple.js')
+const { EmojiAPI } = require("emoji-api")
+const emoji = new EmojiAPI()
+const ggs = require('google-it')
+const googleImage = require('g-i-s')
+const yts = require( 'yt-search')
+const zee = require('api-alphabot')
+const { removeBackgroundFromImageFile } = require('remove.bg')
+
+//══════════[ Lib ]══════════//
+
+const { fetchJosn, fetchText } = require('./lib/fetcher')
+const { color, bgcolor } = require('./lib/color')
+const { wait, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, start, info, success, close } = require('./lib/functions')
+const setting = JSON.parse(fs.readFileSync('./setting/setting.json'))
+const apikey = JSON.parse(fs.readFileSync('./setting/apikey.json'))
+const { lirikLagu } = require('./lib/lirik.js')
+const { herolist } = require('./lib/herolist.js')
+const { herodetails } = require('./lib/herodetail.js')
+const { pinterest } = require('./lib/pinterest')
+const { uploadimg, upload } = require('./lib/uploadimg')
+const { uploadImages } = require('./lib/uploadimage')
+const { mediafireDl } = require('./lib/mediafire.js')
+
+//══════════[ Leveling and Rpg ]══════════//
+
+let { isLimit, limitAdd, getLimit, giveLimit, addBalance, kurangBalance, getBalance} = require("./lib/limit");
+let { checkPetualangUser, addInventori, addBesi, sellBesi, getBesi, addDm, sellDm, getDm, addEmas, sellEmas, getEmas, addFish, sellFish, getFish } = require("./lib/rpgfunction");
+let { addLevelingId, addLevelingLevel, addLevelingXp, getLevelingId, getLevelingLevel, getLevelingXp } = require("./lib/lvlfunction");
